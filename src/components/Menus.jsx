12f7e9a487317ebe2ac6f68450793b890ad1fc83
mohiturkude8237 <mohiturkude8237@gmail.com>
2025-06-06 20:40:27 +0530

    Menu Updated

---
+++ src/components/Menus.jsx
@@ -0,0 +1,39 @@
+import { title } from 'framer-motion/client'
+import React from 'react'
+
+const MenusData=[
+    {
+        id:1,
+        title:"Fresh Red Apples",
+        link:"/",
+        price:"$3.99",
+    },
+    {
+        id:2,
+        title:"Fresh Oranges",
+        link:"/",
+        price:"$4.99",
+    },
+    {
+        id:3,
+        title:"Fresh Avocado",
+        link:"/",
+        price:"$5.99",
+    },
+    {
+        id:4,
+        title:"Fresh Cherries",
+        link:"/",
+        price:"$2.99"
+    }
+]
+
+const Menus = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Menus;
