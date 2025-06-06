2449c2e59ec707a08029aba3cba52de86f8a0bd4
mohiturkude8237 <mohiturkude8237@gmail.com>
2025-06-06 18:59:20 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,30 @@
-// import React from "react";
-// import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Products from "./pages/Products";
-// import About from "./pages/About";
-// import Shop from "./pages/Shop";
-// import Contacts from "./pages/Contacts";
-// import Navbar1 from "./components/Navbar";
-// import ResponsiveMenu from "./components/ResponsiveMenu";
-// import Hero1 from "./components/Hero";
-
-// const App = () => {
-//   return (
-//     <main className="overflow-x-hidden">
-//       <Navbar />
-
-//       {/* <Routes>
-//         <Route path='/' element={<Home />} />
-//         <Route path='/products' element={<Products />} />
-//         <Route path='/about' element={<About />} />
-//         <Route path='/shop' element={<Shop />} />
-//         <Route path='/contacts' element={<Contacts />} />
-//       </Routes> */}
-//       <Hero/>
-//     </main>
-//   );
-// };
-
-// export default App;
-
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
+import Contacts from "./pages/Contacts";
+import Navbar from "./components/Navbar";
+import ResponsiveMenu from "./components/ResponsiveMenu";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div>
-      hello
-    </div>
-  )
-}
+    <main className="overflow-x-hidden">
+      <Navbar/>
 
-export default App
+      {/* <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/contacts' element={<Contacts />} />
+      </Routes> */}
+      <Hero/>
+    </main>
+  );
+};
+
+export default App;
+
