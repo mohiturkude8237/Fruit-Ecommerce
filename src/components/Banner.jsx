7a0c4c49899a7e9ffb81d419367280fd294cfcda
mohiturkude8237 <mohiturkude8237@gmail.com>
2025-06-07 12:51:18 +0530

    Title Updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { section } from "framer-motion/client";
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Banner = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section>
+      <div className="container">
+        {/* Banner Image */}
+        <div>
+          <img src={assets.fruits_splash} alt="" className="w-[300px]" />
+        </div>
+        {/* Banner Info */}
+      </div>
+    </section>
+  );
+};
 
-export default Banner
+export default Banner;
