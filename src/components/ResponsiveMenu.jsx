7f50f0a50ae80667911a70047def00045ada7c9b
mohiturkude8237 <mohiturkude8237@gmail.com>
2025-06-07 12:47:47 +0530

    Banner.jsx updated

--- src/components/ResponsiveMenu.jsx
+++ src/components/ResponsiveMenu.jsx
@@ -14,10 +14,10 @@ const ResponsiveMenu = ({open}) => {
           className='absolute  top-20 left-0 w-full h-screen z-20'>
             <div className='text-xl font-semibold uppercase bg-red-600 text-white py-10 m-6 rounded-3xl '>
               <ul className='flex flex-col items-center gap-10'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Services</li>
-                <li>Contact</li>
+                <li> <a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/service">Services</a></li>
+                <li><a href="/contact">Contact</a></li>
               </ul>
             </div>
           </motion.div>
