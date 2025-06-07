78c719ec7385ff16b5caad23e78988214705eb85
mohiturkude8237 <mohiturkude8237@gmail.com>
2025-06-07 09:10:44 +0530

    Fruits menu images added

--- src/components/Menus.jsx
+++ src/components/Menus.jsx
@@ -1,38 +1,55 @@
-import { title } from 'framer-motion/client'
+import { img, section, title } from 'framer-motion/client'
 import React from 'react'
-
+import apple from '../assets/fruits/apple.png'
+import orange from '../assets/fruits/orange.png'
+import avocado from '../assets/fruits/avocado.png' 
+import cherry from '../assets/fruits/cherry.png'
 const MenusData=[
     {
         id:1,
         title:"Fresh Red Apples",
         link:"/",
         price:"$3.99",
+        img: apple,
     },
     {
         id:2,
         title:"Fresh Oranges",
         link:"/",
         price:"$4.99",
+        img: orange,
     },
     {
         id:3,
         title:"Fresh Avocado",
         link:"/",
         price:"$5.99",
+        img: avocado,
     },
     {
         id:4,
         title:"Fresh Cherries",
         link:"/",
-        price:"$2.99"
+        price:"$2.99",
+        img: cherry,
     }
 ]
 
 const Menus = () => {
   return (
-    <div>
-      
-    </div>
+    <section>
+        <div className="container">
+            <h1 className='text-2xl font-bold text-left pb-10 border uppercase'>Our Menu</h1>
+                {MenusData.map((menu) => (
+                    <div>
+                        <img src={menu.img} alt="" className='w-[60px] mb-4 scale-125 transform -translate-y-6'/>
+                        </div>
+                ))}
+                </div>
+
+
+    
+    </section>
   )
 }
 
