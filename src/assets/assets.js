e6d65ef1576dd08a378abb9717fbbf8ca50a7cb8
mohiturkude8237 <mohiturkude8237@gmail.com>
2025-06-07 12:47:16 +0530

    fruits_splash image imported.

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,11 +4,14 @@ import logo1 from './logo1.jpg'
 import logo3 from './logo3.png'
 import fruit_plate from './fruit_plate.png'
 import leaf from './leaf.png'
+import fruits_splash from './fruits_splash.png'
 export const assets={
     banner_bg,
     logo
     ,logo1,
     logo3,
     fruit_plate,
-    leaf
+    leaf,
+    fruits_splash
+    
 }
